4a55246f2248e73e964a8d455075c1e4f748f19b
Astro <rgrxmahabub.official@gmail.com>
2023-11-25 01:19:56 +0600

    Vercel Deploy

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express, { Application, Request, Response }  from "express"
 import cors from "cors"
 
 const app:Application = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req :Request, res:Response) => {
-  res.send('Hello World!')
+  res.send('Welcome Users!')
 })
 
 export default app
